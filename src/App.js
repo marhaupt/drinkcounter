4793fd4fa3ce6bc72d37acf4217520ed2093536a
Martin Haupt <martin23haupt@gmail.com>
2017-11-27 20:48:01 +0100

    initial commit, got rid of most useless things

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="app">
+        DRINK UP!
+      </main>
+    );
+  }
+}
+
+export default App;
