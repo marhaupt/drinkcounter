df2bb90dace655213a4f314e3f204dc39ca9924a
Martin Haupt <martin23haupt@gmail.com>
2017-11-27 21:21:12 +0100

    basic funcionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    drinks: []
+  };
+
+  addDrink = _ => {
+    const now = new Date().getTime();
+
+    const updatedDrinks = [...this.state.drinks, now];
+
+    this.setState({
+      drinks: updatedDrinks
+    });
+  };
+
   render() {
     return (
-      <main className="app">
-        DRINK UP!
+      <main>
+        <button className="drink-button" onClick={this.addDrink}>
+          <img src="./imgs/beer.svg" alt="" />
+        </button>
+        <p className="drink-count">{this.state.drinks.length}</p>
       </main>
     );
   }
