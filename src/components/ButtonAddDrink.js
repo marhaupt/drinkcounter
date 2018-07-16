cc90902f9d8089b929404d34f4bcc5d0d17157e2
Martin Haupt <martin23haupt@gmail.com>
2018-07-16 10:12:01 +0200

    icons

--- src/components/ButtonAddDrink.js
+++ src/components/ButtonAddDrink.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Beer from '../icons/Beer';
 
 const ButtonAddDrink = ({ addDrink }) => (
   <button className="drink-button" onClick={addDrink} aria-label="add drink">
-    <img src="./imgs/beer.svg" alt="" />
+    <Beer color="#e7a946" />
   </button>
 );
 
---
+++ src/icons/Wine.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default ({ color }) => (
+  <svg
+    viewBox="0 0 33 41"
+    stroke={color}
+    strokeWidth="1px"
+    fill="none"
+    fillRule="evenodd"
+    clipRule="evenodd"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeMiterlimit="1.5"
+  >
+    <g id="wine">
+      <path d="M21.529,1.989c5.632,21.252 -0.056,22.657 -4.654,22.735c-0.019,13.052 0.209,12.034 5.84,14.384l-0.155,-0.011c-1.634,-0.112 -11.228,-0.107 -12.866,0.011l0.132,-0.055c5.502,-2.279 5.834,-1.406 5.708,-14.329c-4.86,-0.104 -10.286,-1.483 -4.654,-22.735" />
+      <path d="M10.187,13.358l11.675,0" />
+    </g>
+  </svg>
+);
